7894271434e03d7c350b13118a83814ddee0f01b
Kevin Ascanio <kevinascanior@gmail.com>
2024-07-17 00:40:14 -0300

    form y responsive menu

---
+++ java/java.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  document.querySelector(".navegationMenu").classList.toggle("show");
+});
+
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  document.querySelector(".main").classList.toggle("show");
+});
